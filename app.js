a78c6bd3d12d5a3b6a2aac8ab1fe4a6046637404
Mouad El Asri <elasri.mouad.2002@gmail.com>
2023-05-31 11:02:11 +0100

    I swear the data is real-time, not just me procrastinating on writing the commit message.

--- app.js
+++ app.js
@@ -1,7 +1,45 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js"
+import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-database.js"
+
 const	inputEl = document.getElementById("input-el");
 const	buttonEl = document.getElementById("button-el");
+const	shoppingListEl = document.getElementById("shopping-list");
+
+const appSettings = {
+	databaseURL : "https://marketmate-b1086-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const listInDB = ref(database, "Shopping List");
 
 buttonEl.addEventListener("click", function() {
 	const	inputValue = inputEl.value;
-	console.log(inputValue);
+	push(listInDB, inputValue); 
+	clearInput();
 });
+
+onValue(listInDB, function(snapshot) {
+	let listItemsArray = Object.entries(snapshot.val());
+	clearShoppingList();
+	for (let i = 0; i < listItemsArray.length; i++) {
+		let currentItemID = listItemsArray[i][0];
+		let currentItemValue = listItemsArray[i][1];
+		addItemToList(currentItemValue);
+	}
+})
+
+function clearShoppingList()
+{
+	shoppingListEl.innerHTML = "";
+}
+
+function clearInput()
+{
+	inputEl.value = "";
+}
+
+function addItemToList(itemValue)
+{
+	shoppingListEl.innerHTML += `<li>${itemValue}</li>`;
+}
